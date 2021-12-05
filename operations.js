958266537ee28ed5e2a43b86a4a9f2a66bc059d4
ali bahmanabadi <alibahmanabadi00@gmail.com>
2021-12-05 12:09:39 +0330

    finish tasks

--- operations.js
+++ operations.js
@@ -4,36 +4,53 @@ const operationResult = document.querySelector('.operationResult')
 operationInput.addEventListener('change', (e) => operations(e.target.value))
 
 export default function operations(ops) {
+  // empty result
+  operationResult.innerHTML = ''
   let result = null
 
+  //split input
   let splitOps = ops.split(' ')
-  splitOps = splitOps.map((item) =>  parseInt(item ))
-  const currentOps = splitOps[splitOps.length - 1]
-  const preventOps = splitOps[splitOps.length - 2]
 
+  //remove character in array
+  const deleteStringInArray = splitOps.filter((item) =>
+    item.replace(/^\D+/g, '')
+  )
 
-  console.log(splitOps)
+  // operation R , D , +
+  splitOps.forEach((item) => {
+    let lastValue = deleteStringInArray[deleteStringInArray.length - 1]
 
-  switch (currentOps) {
-    case 'D':
-      const value = parseInt(preventOps) * 2
-      splitOps.pop()
-      splitOps.push(value)
+    if (item === 'D') {
+      const doubleLastValue = (parseInt(lastValue) * 2).toString(10)
+      lastValue = doubleLastValue
+      deleteStringInArray.push(doubleLastValue)
+    } else if (item === 'R') {
+      deleteStringInArray.pop()
+    } else if (item === '+') {
+      const preventValue = deleteStringInArray[deleteStringInArray.length - 2]
+      console.log(preventValue)
+      console.log(lastValue)
+      const sum = (parseInt(preventValue) + parseInt(lastValue)).toString()
+      deleteStringInArray.push(sum)
+    }
+  })
 
-      console.log(splitOps)
-      break
-    case 'R':
+  // show result in new line
+  let final = []
+  deleteStringInArray.map((item) => {
+    final.push(item)
+    result = JSON.stringify(final)
+    operationResult.innerHTML += result + '<br>'
+  })
 
-      break
-    case '+':
-      break
-    default:
-       splitOps
-  }
-  
+  // sum total array
+  const total = deleteStringInArray.reduce(
+    (acc, cur) => parseInt(acc) + parseInt(cur),
+    0
+  )
 
-  result = JSON.stringify(splitOps)
+  //add total to result
+  operationResult.innerHTML += total + '<br>'
 
-  operationResult.textContent = result 
   return result
 }
